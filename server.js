e28f8f1371c68fc0ac5f8d93b826fd59f669b673
Victor Leitão <victorleitao1991@hotmail.com>
2024-01-09 18:31:48 -0300

    index.html moved to root

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res) => {
-	res.status(404);
-	res.send(`<h1>Error 404: Resource not found</h1>`);
+app.get('/public', (req, res) => {
+	app.use(index);
 });
 
-app.listen(3000, () => {
-	console.log('App listening on port 3000');
-});
+app.listen(3000);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req, res) => {
+// 	res.status(404);
+// 	res.send(`<h1>Error 404: Resource not found</h1>`);
+// });
 
 // app.set('view-engine');
-
-// app.get('/', (req, res) => {
-// 	res.render('index.ejs');
-// });
 
 // app.post('/index', (req, res) => {});
