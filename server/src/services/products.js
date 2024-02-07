2f8cf76958de6fad7303e8a10a382b498a6dc7ee
Victor Leitão <victorleitao1991@hotmail.com>
2024-02-07 03:17:13 -0300

    Vercal and MongoDB Atlas integration concluded

--- server/src/services/products.js
+++ server/src/services/products.js
@@ -4,21 +4,20 @@ const productCollection = require('../models/product');
 const categoryCollection = require('../models/category');
 
 router.get('/', async (req, res) => {
-	// let filter = {};
-	// if (req.query.categories) {
-	// 	filter = { category: req.query.categories.split(',') };
-	// }
+	let filter = {};
+	if (req.query.categories) {
+		filter = { category: req.query.categories.split(',') };
+	}
 
-	// const productList = await productCollection
-	// 	.find(filter)
-	// 	.populate('category')
-	// 	.select('-__v -dateCreated');
+	const productList = await productCollection
+		.find(filter)
+		.populate('category')
+		.select('-__v -dateCreated');
 
-	// if (!productList) {
-	// 	res.status(500).json({ success: false });
-	// }
-	// res.send(productList);
-	res.send('Olar, produtos!');
+	if (!productList) {
+		res.status(500).json({ success: false });
+	}
+	res.send(productList);
 });
 
 router.get('/search', async (req, res) => {
